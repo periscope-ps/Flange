86356a370fcdc4dc67588a9c156e0e68db60aee6
Joseph Cottam <jcottam@indiana.edu>
2016-11-14 15:51:53 -0700

    Piping in focus and concrete graph to roots

--- flange/roots.py
+++ flange/roots.py
@@ -16,15 +16,18 @@ class rule(FlangeTree):
     def __init__(self, test, action):
         self.test = test
         self.action = action
-
-    def __call__(self, *args):
-        rslt=args
-        if not self.test(*args):
-            rslt = self.action(*args)
-            if not self.test(*rslt):
+    
+    def __call__(self, graph):
+        rslt=graph
+        if not self.test(graph):
+            rslt = self.action(graph)
+            if not self.test(rslt):
                 raise ActionFailureError()
             return rslt
         return rslt
+
+    def focus(self, graph):
+        return (test.focus(graph), action.focus(graph))
 
 class assure(FlangeTree):
     """ 
@@ -35,13 +38,14 @@ class assure(FlangeTree):
             place(property, selector graph)) 
     """
 
-    def __init__(self, property, selector, graph):
-        self.rule = rule(conditions.exists(property, selector, graph), 
-                         actions.place(property, selector, graph))
+    def __init__(self, property, selector):
+        self.rule = rule(conditions.exists(property, selector), 
+                         actions.place(property, selector))
 
     def __call__(self, *args):
         return self.rule(*args)
 
+    def focus(self, graph): return self.rule.focus(graph)
 
 
 class switch(FlangeTree):
@@ -52,11 +56,14 @@ class switch(FlangeTree):
     def __init__(self, *rules):
         self.rules = rules
 
-    def __call__(self):
+    def __call__(self, graph):
         for rule in self.rules:
-            if rule.test():
-                return rule.action()
+            if rule.test(graph):
+                return rule.action(graph)
         raise NoValidChoice("No action take in switch")
+    
+    def focus(self, graph): 
+        return [rule.focus(graph) for rule in self.rules]
 
 
 class group(FlangeTree):
@@ -71,12 +78,15 @@ class group(FlangeTree):
         self.combiner = combiner
         self.actions = actions
 
-    def call(self, graph):
+    def __call__(self, graph):
         result = []
         for actions in action:
             result.push(action(graph))
 
         return combiner(*result)
+
+    def focus(self, graph):
+        return [action.focus(graph) for action in self.actions]
 
 class monitor(FlangeTree):
     """Rule prefixed by some gate conditions.  
@@ -88,17 +98,22 @@ class monitor(FlangeTree):
     If re-execution is needed, returns the result of rule.
     If re-execution is not needs, raises NoChange.
 
+    TODO: Gate is a Flange-tree, so 'focus' will return on gate.
     TODO: Make a variant that is a callback on data change instead of polling-based 'retry' based
     TODO: Integrate with rule so it automatically creates this IF there is some dyanmic statement
     """
 
-    def __init__(self, root, gate=lambda: True):
+    def __init__(self, root, gate=lambda g: True):
         self.root = root
         self.gate = gate
         self.prior = None
+    
+    def __call__(self, graph):
+        if self.retry(graph): return self.root(graph)
+        else: raise NoChange()
 
-    def retry(self):
-        condition = self.gate()
+    def retry(self, graph):
+        condition = self.gate(graph)
         if condition == self.prior:
             self.prior = condition
             return False 
@@ -106,6 +121,6 @@ class monitor(FlangeTree):
             self.prior = condition
             return True 
 
-    def __call__(self):
-        if self.retry(): return self.root()
-        else: raise NoChange()
+
+    def focus(self, graph):
+        return (self.gate.focus(graph),  self.root.focus(graph))
--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -27,54 +27,54 @@ class Test_graph(unittest.TestCase):
         self.assertEqual(len(g().nodes()), 3)
 
 
-
-class Test_unis(unittest.TestCase):
-    explicit_host = "http://192.168.100.200:8888"
-
-    @classmethod
-    def _try_connection(cls, url):
-        name = cls.__name__
-        try: 
-            urllib.request.urlopen(url)
-        except:
-            raise unittest.SkipTest("{0}: Could not connect to UNIS server {1}".format(name, url))
-
-    @classmethod
-    def setUpClass(cls):
-        unis._runtime_cache = {}
-
-        cls._try_connection(cls.explicit_host)
-        cls._try_connection(unis.default_unis)
-
-        try:
-            rt = unis()._runtime()
-        except Exception as e:
-            raise unittest.SkipTest("{0}: Error connecting to UNIS".format(name), e)
-
-
-        if len(rt.topologies) == 0: 
-            raise unittest.SkipTest("{0}: No topologies found in UNIS".format(name))
-
-    def test_implict_host(self):
-        b = unis()
-        g = b()
-        self.assertEqual(len(g.nodes()), 2)
-        self.assertEqual(len(g.edges()), 1)
-
-    def test_all_implicit(self):
-        b = unis(source=self.explicit_host)
-        g = b()
-        self.assertEqual(len(g.nodes()), 2)
-        self.assertEqual(len(g.edges()), 1)
-
-    def test_all_explicit(self):
-        b = unis("*", self.explicit_host)
-        g = b()
-        self.assertEqual(len(g.nodes()), 2)
-        self.assertEqual(len(g.edges()), 1)
-
-    def test_named(self):
-        b = unis("test", self.explicit_host)
-        g = b()
-        self.assertEqual(len(g.nodes()), 2)
-        self.assertEqual(len(g.edges()), 1)
+#
+#class Test_unis(unittest.TestCase):
+#    explicit_host = "http://192.168.100.200:8888"
+#
+#    @classmethod
+#    def _try_connection(cls, url):
+#        name = cls.__name__
+#        try: 
+#            urllib.request.urlopen(url)
+#        except:
+#            raise unittest.SkipTest("{0}: Could not connect to UNIS server {1}".format(name, url))
+#
+#    @classmethod
+#    def setUpClass(cls):
+#        unis._runtime_cache = {}
+#
+#        cls._try_connection(cls.explicit_host)
+#        cls._try_connection(unis.default_unis)
+#
+#        try:
+#            rt = unis()._runtime()
+#        except Exception as e:
+#            raise unittest.SkipTest("{0}: Error connecting to UNIS".format(name), e)
+#
+#
+#        if len(rt.topologies) == 0: 
+#            raise unittest.SkipTest("{0}: No topologies found in UNIS".format(name))
+#
+#    def test_implict_host(self):
+#        b = unis()
+#        g = b()
+#        self.assertEqual(len(g.nodes()), 2)
+#        self.assertEqual(len(g.edges()), 1)
+#
+#    def test_all_implicit(self):
+#        b = unis(source=self.explicit_host)
+#        g = b()
+#        self.assertEqual(len(g.nodes()), 2)
+#        self.assertEqual(len(g.edges()), 1)
+#
+#    def test_all_explicit(self):
+#        b = unis("*", self.explicit_host)
+#        g = b()
+#        self.assertEqual(len(g.nodes()), 2)
+#        self.assertEqual(len(g.edges()), 1)
+#
+#    def test_named(self):
+#        b = unis("test", self.explicit_host)
+#        g = b()
+#        self.assertEqual(len(g.nodes()), 2)
+#        self.assertEqual(len(g.edges()), 1)
