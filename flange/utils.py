1f8dac95fa6a1ceed5db166da62c2257f27e88ca
Joseph Cottam <jcottam@indiana.edu>
2016-12-05 16:47:25 -0500

    Coloring to distinguish node types.

--- flange/utils.py
+++ flange/utils.py
@@ -1,10 +1,15 @@
 import networkx as nx
 from ._internal import *
 
-def draw(g, pos=None, ax=None):
+def draw(g, pos=None, ax=None, **layout_args):
+    color_map = {"node": "r", "link": "b"}
     g = g if not isinstance(g, FlangeTree) else g()
+    pos=nx.spring_layout(g, **layout_args) if pos is None else pos
 
-    pos=nx.spring_layout(g) if pos is None else pos
-    nx.draw(g, pos=pos, ax=ax)
+    types = nx.get_node_attributes(g, "_type")
+    colors = [color_map.get(types[node], "k") 
+              for node in g.nodes()]
+
+    nx.draw(g, pos=pos, ax=ax, node_color=colors)
     nx.draw_networkx_labels(g, pos=pos, ax=ax)
 
