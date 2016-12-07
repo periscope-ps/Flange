b295488ad2e35b6e311db4480c06f4b1e5a072c9
Joseph Cottam <jcottam@indiana.edu>
2016-12-07 16:46:41 -0500

    Moving vertex monkey-patch to a LESS import sensitive place

--- flange/__init__.py
+++ flange/__init__.py
@@ -4,3 +4,21 @@ from .graphs import *
 from .conditions import *
 from .roots import *
 from .actions import *
+
+
+import networkx as nx
+# Flange uses vertex/edge to refer to graph items and node/link to refer to network things.
+# So we monkey patch in a vertices-named methods for all the graph classes
+# Patching here seems to work when importing any flange modules...
+# and any subsequent use of networkx by any module.
+# This might not be good form, and it might be mildly dangerous, since
+# these methods could theoretically be overwitten subsequently by other module imports.
+# Live (slightly) dangerously
+
+nx.classes.Graph.vertices = lambda self: self.nodes()
+nx.classes.Graph.vertices_iter = lambda self: self.nodes_iter()
+nx.classes.Graph.has_vertex = lambda self, vertex: self.has_node(vertex)
+nx.classes.Graph.number_of_vertices = lambda self: self.number_of_nodes()
+nx.classes.Graph.vertices_with_self_loops = lambda self: self.nodes_with_self_loops()
+nx.classes.Graph.add_vertex = lambda self, vertex: self.add_node(vertex)
+nx.classes.Graph.add_vertices_from = lambda self, iterable: self.add_nodes_from(iterable)
--- flange/graphs.py
+++ flange/graphs.py
@@ -6,17 +6,6 @@ from functools import reduce
 
 from ._internal import FlangeTree, autotree
 
-
-# Flange uses vertex/edge to refer to graph items and node/link to refer to network things.
-# So we monkey patch in a vertices-named methods for all the graph classes
-nx.classes.Graph.vertices = lambda self: self.nodes()
-nx.classes.Graph.vertices_iter = lambda self: self.nodes_iter()
-nx.classes.Graph.has_vertex = lambda self, vertex: self.has_node(vertex)
-nx.classes.Graph.number_of_vertices = lambda self: self.number_of_nodes()
-nx.classes.Graph.vertices_with_self_loops = lambda self: self.nodes_with_self_loops()
-nx.classes.Graph.add_vertex = lambda self, vertex: self.add_node(vertex)
-nx.classes.Graph.add_vertices_from = lambda self, iterable: self.add_nodes_from(iterable)
-
 class unis(FlangeTree):
     "Retrieves a graph from a UNIS server."
     default_unis = "http://192.168.100.200:8888"
