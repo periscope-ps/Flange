68b64037fe191b0a6bb1fdbec170dd07f54d48f4
Joseph Cottam <jcottam@indiana.edu>
2016-12-13 16:14:09 -0500

    Layout documentation and plumbing to show deltas

--- flange/utils.py
+++ flange/utils.py
@@ -3,6 +3,15 @@ from ._internal import *
 from .graphs import islink, isnode
 
 def pick_labels(g, strategy):
+    """Return the lables for a graph. 
+
+    Strategies --
+    * nodes: Label all verticies that represent graph nodes
+    * links: Label all verticies that reprsent links
+    * auto: Label nodes if there are any.  Label links otherwise
+    """
+
+
     if strategy == "auto":
         links = [v for v in g.vertices() if islink(v,g)]
         nodes = [v for v in g.vertices() if isnode(v,g)]
@@ -25,7 +34,9 @@ def pick_labels(g, strategy):
         return {v: v if not isnode(v,g) else ""
                 for v in g.vertices()}
 
-def draw(src, pos=None, ax=None, iterations=2, label='auto', **layout_args):
+def draw(src, pos=None, ax=None, *, delta=None, iterations=2, label='auto', **layout_args):
+    "Layout and draw a graph."
+
     layout_args["iterations"] = iterations 
     g = src if (not isinstance(src, FlangeTree)) else src()
 
@@ -47,3 +58,35 @@ def draw(src, pos=None, ax=None, iterations=2, label='auto', **layout_args):
 
     nx.draw(g, pos=pos, ax=ax, node_size=sizes, node_color=colors)
     nx.draw_networkx_labels(g, labels=labels, pos=pos, ax=ax)
+
+
+def show(flanglet, graph, *, size=(8,10)):
+    "Draw a sequence of graphs to reprsent prcoessing" 
+
+    before = graph().copy()
+    foci = flanglet.focus(before.copy())
+    after = flanglet(before.copy())
+
+    pos = nx.spring_layout(before)
+    
+    fig = plt.gcf()
+    fig.set_figheight(size[0])
+    fig.set_figwidth(size[1])
+    
+    cols = max(len(foci), 6)
+    rows = 4
+    ax_before = plt.subplot2grid((rows, cols), (0,0), colspan=cols//2, rowspan=3)
+    ax_after = plt.subplot2grid((rows, cols), (0,cols//2), colspan=cols//2, rowspan=3)
+        
+    nx.draw(before, pos=pos, ax=ax_before)
+    nx.draw_networkx_labels(before, pos=pos, ax=ax_before)
+
+    for (i, sub) in enumerate(foci):
+        ax_select = plt.subplot2grid((rows, cols), (3, i))
+        nx.draw(sub, pos=pos, ax=ax_select)
+        nx.draw_networkx_labels(sub, pos=pos, ax=ax_select)
+
+    nx.draw(after, pos=pos, ax=ax_after)
+    nx.draw_networkx_labels(after, pos=pos, ax=ax_after)
+    
+    return fig
