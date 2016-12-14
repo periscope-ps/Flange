b6747c654a895854c1545a3295f4d7ac379f41bf
Joseph Cottam <jcottam@indiana.edu>
2016-12-14 10:59:48 -0500

    Computes delta dictionary

--- flange/utils.py
+++ flange/utils.py
@@ -2,6 +2,54 @@ import networkx as nx
 from ._internal import *
 from .graphs import islink, isnode
 
+
+from collections import defaultdict
+def delta(old, new):
+    """
+    Computes the delta between two nodes.
+    Delta is a change in attributes or entity count (vertex added or deleted)
+    or structure (links added or deleted).
+    
+    old -- original graph
+    new -- updated graph
+    returns -- dictionary where keys are nodes that were changed
+           and values are a description of the change
+    """
+    
+    delta = defaultdict(list)
+    def extend(key, val):
+        entries = delta[key]
+        entries.append(val)
+        
+    def cmp(dict1, dict2):
+        diff = {key:val for (key, val) in dict2.items()
+                if dict2[key] is not dict1[key]}
+        
+        keys1 = set(dict1.keys())
+        keys2 = set(dict2.keys())
+        removed = {key:"prop removed" 
+                   for key in keys1.difference(keys2)}
+        added = {key:"prop added" 
+                 for key in keys2.difference(keys1)}
+        return {**diff, **removed, **added}
+        
+    
+    for v in old.vertices():
+        if v not in new.vertices():
+            extend(v, ("deleted"))
+            
+    for v in new.vertices():
+        if v not in old.vertices():
+            extend(v, ("added"))
+        else:
+            diff = cmp(old.vertex[v], new.vertex[v])
+            if diff:
+                extend(v, ("props", diff))
+            
+    return delta
+
+
+
 def pick_labels(g, strategy):
     """Return the lables for a graph. 
 
@@ -10,7 +58,6 @@ def pick_labels(g, strategy):
     * links: Label all verticies that reprsent links
     * auto: Label nodes if there are any.  Label links otherwise
     """
-
 
     if strategy == "auto":
         links = [v for v in g.vertices() if islink(v,g)]
@@ -34,7 +81,7 @@ def pick_labels(g, strategy):
         return {v: v if not isnode(v,g) else ""
                 for v in g.vertices()}
 
-def draw(src, pos=None, ax=None, *, delta=None, iterations=2, label='auto', **layout_args):
+def draw(src, pos=None, ax=None, *, delta=[], iterations=2, label='auto', **layout_args):
     "Layout and draw a graph."
 
     layout_args["iterations"] = iterations 
@@ -45,8 +92,9 @@ def draw(src, pos=None, ax=None, *, delta=None, iterations=2, label='auto', **la
         pos=nx.spring_layout(g, pos=pos, **layout_args)
 
     def color(vertex):
-        if isnode(vertex, g): return "r"
-        if islink(vertex, g): return "b"
+        if vertex in delta: return "#439B00"  #yellow: "#FFFF00"
+        if isnode(vertex, g): return "#F85565"
+        if islink(vertex, g): return "#4A7BBB"
         return "w"
 
     types = nx.get_node_attributes(g, "_type")
@@ -58,7 +106,6 @@ def draw(src, pos=None, ax=None, *, delta=None, iterations=2, label='auto', **la
 
     nx.draw(g, pos=pos, ax=ax, node_size=sizes, node_color=colors)
     nx.draw_networkx_labels(g, labels=labels, pos=pos, ax=ax)
-
 
 def show(flanglet, graph, *, size=(8,10)):
     "Draw a sequence of graphs to reprsent prcoessing" 
@@ -90,3 +137,6 @@ def show(flanglet, graph, *, size=(8,10)):
     nx.draw_networkx_labels(after, pos=pos, ax=ax_after)
     
     return fig
+
+
+
