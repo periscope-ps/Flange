543fb30aa705745d056eb05d512bb554f6ab17c1
Jeremy Musser <jemusser@indiana.edu>
2017-10-28 13:08:14 -0400

    temporary removal of subscription handler

--- flanged/handlers/__init__.py
+++ flanged/handlers/__init__.py
@@ -3,5 +3,5 @@ from flanged.handlers.auth import AuthHandler
 from flanged.handlers.compiler import CompileHandler
 from flanged.handlers.validate import ValidateHandler
 from flanged.handlers.sketches import SketchHandler
-from flanged.handlers.subscription import SubscriptionHandler
+#from flanged.handlers.subscription import SubscriptionHandler
 from flanged.handlers.base import SSLCheck
